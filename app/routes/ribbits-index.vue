634a71becba4ed70379835d53dbf3f005b5abf9c
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-03-20 16:10:54 -0500

    created ribbits and users pages

--- app/routes/ribbits-index.vue
+++ app/routes/ribbits-index.vue
@@ -1,5 +1,22 @@
 <template lang="html">
   <div class="">
+    <div class="section">
+      <div class="container">
+        <div class="ribbits-heading">
+          <h1>Recent Ribbits</h1>
+        </div>
+
+        <div class="ribbit">
+          <div class="ribbit__heading">
+            <h1>New Ribbit</h1>
+          </div>
+        <form @submit.prevent="submit" class="ribbit-form">
+          <div class="ribbit-form__content"></div>
+        </form>
+
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
--- app/routes/users-index.vue
+++ app/routes/users-index.vue
@@ -1,19 +1,51 @@
 <template lang="html">
   <div class="">
+    <div class="section">
+      <div class="container">
+        <div class="users-heading">
+          <h1>Users</h1>
+        </div>
 
+        <div class="users-grid">
+
+          <div class="users-grid__item">
+            <div class="user">
+              <img src="#" alt="" class="user-image">
+              <div class="user-info">
+                <p class="user-info__name">{{ user.username }}</p>
+                <a href="" class="user-info__email">{{ user.email }}</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import store from '../store';
+import userResource from '../resources/user';
 export default {
   name: 'Users',
   data() {
     return {
+      users: this.$select('users'),
     };
   },
 
   methods: {
+  },
 
+  created() {
+    const {
+      actionCreators: {
+        findAll,
+      },
+    } = userResource;
+    store.dispatch(findAll());
   },
 };
 </script>
