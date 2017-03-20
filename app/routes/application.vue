de60e947a888f3c2a325148dc065f352ac835fa2
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-03-20 13:43:41 -0500

    setup application and styled navbar

---
+++ app/routes/application.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="">
+    <div class="top-nav">
+      <div class="navbar">
+        <p>Ribbeter</p>
+        <ul class="nav-right">
+          <li class="nav-right__item">Ribbets</li>
+          <li class="nav-right__item">Frogs</li>
+          <li class="nav-right__item">Login</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Application',
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
